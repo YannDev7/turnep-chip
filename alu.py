64b50b8bf25064855f47330198b1ea94b4625089
YannDev7 <yviegas@clipper.ens.fr>
2024-12-10 13:59:50 +0100

    full adder

---
+++ alu.py
@@ -0,0 +1,5 @@
+from carotte.lib_carotte import *
+
+def full_adder(a: Variable, b: Variable, c: Variable) -> typing.Tuple[Variable, Variable]:
+    tmp = a ^ b
+    return (tmp ^ c, (tmp & c) | (a & b))
