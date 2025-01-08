1039b60fd6fab458dbebcd1067a38c55253be2f2
Aimeric1 <aimeric.duchemin@gmail.com>
2025-01-08 22:14:30 +0100

    Cohérence

--- alu.py
+++ alu.py
@@ -34,13 +34,13 @@ class ALU:
         creeret = [And(a[i], b[i]) for i in range(a.bus_size)]
         propret = [Xor(a[i], b[i]) for i in range(a.bus_size)]
         def propagee(i, j): #il faut j>i, dit si la retenue éventuelle en j a été propagée jusqu'en i-1
-            return giga_and(concatlst([propret[i:j]]))
+            return giga_and(propret[i:j])
         clst = [] #liste des retenues (sauf celle de la sortie)
         for i in range(a.bus_size-1):
-            clst.append(giga_xor(concatlst([And(propagee(i+1, j), creeret[j]) for j in range(i+2, a.bus_size-1)] +[creeret[i+1], And(propagee(i+1, a.bus_size), c)])))
+            clst.append(giga_xor([And(propagee(i+1, j), creeret[j]) for j in range(i+2, a.bus_size-1)] +[creeret[i+1], And(propagee(i+1, a.bus_size), c)]))
         clst.append(c)
-        sortie = Xor(Xor(concatlst(clst), a), b)
-        csortie = giga_xor(concatlst([And(propagee(0, j), creeret[j]) for j in range(1, a.bus_size-1)]+[creeret[0], And(propagee(0, a.bus_size), c)])) #pourrait aussi être un or
+        sortie = Xor(Xor(clst, a), b)
+        csortie = giga_xor([And(propagee(0, j), creeret[j]) for j in range(1, a.bus_size-1)]+[creeret[0], And(propagee(0, a.bus_size), c)]) #pourrait aussi être un or
         return (sortie, csortie)
 
     def several_adder(self, lst): #profondeur O(logn+logm) pour ajouter m nombres de taille n; ne sort pas, ne prend pas de retenue
@@ -50,7 +50,7 @@ class ALU:
 
     def _several_adder(self, lst2):
         if len(lst2[0]) == 2:
-            return self.carry_lookahead_adder(concatlst([lst2[i][0] for i in range(len(lst2))]), concatlst([lst2[i][1] for i in range(len(lst2))]))
+            return self.carry_lookahead_adder([lst2[i][0] for i in range(len(lst2))], [lst2[i][1] for i in range(len(lst2))])
         nbtruples, reste = divmod(len(lst2[0]), 3)
         suivants = [list() for _ in range(len(lst2))]
         for i in range(nbtruples):
@@ -103,11 +103,11 @@ class ALU:
         return Not(a)
 
     def lshift(self, a, b, nuage): #pas trouvé de meilleure façon de faire qu'un gros multiplexeur. Ne marche que sur du 32bits
-        return Mux(giga_or(b[:26]), giga_mux(b[26:], [klshift(a, k) for k in range(32)]), Constant("0"*32))
+        return Mux(giga_or(listifier(b[:26])), giga_mux(b[26:], [klshift(a, k) for k in range(32)]), Constant("0"*32))
         #derniers 6 bits suffisent à compter jusqu'à 32. Le giga_or détermine si b plus grand que 32.
     
     def rshift(self, a, b, nuage):
-        return Mux(giga_or(b[:26]), giga_mux(b[26:], [krshift(a, k) for k in range(32)]), Constant("0"*32))
+        return Mux(giga_or(listifier(b[:26])), giga_mux(b[26:], [krshift(a, k) for k in range(32)]), Constant("0"*32))
 
     def mul(self, a, b, nuage): #profondeur O((logn)^2)
         def giga_adder(lst):
